e728c7b58c6fcad6640bcc95de00a4c229ba5f36
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-09-01 16:10:27 -0300

    feat: StyleSheet added

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,18 +1,27 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState(10);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={{ textAlign: 'center', fontSize: 40 }}>
-        Contador: {counter}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {counter}</Text>
       <TouchableOpacity onPress={() => setCounter(counter + 1)}>
         <Text>+</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+  },
+});
